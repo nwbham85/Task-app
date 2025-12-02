3e1c045747fed8f56b46bd29cea5fcb05aa5a457
Nathan W <nwinslow@gmail.com>
2025-12-02 13:34:05 -0600

    added css and js logic to signup page

--- public/signup.js
+++ public/signup.js
@@ -9,19 +9,25 @@ const button = document.getElementById('submit');
 button.addEventListener('submit', createUser);
 
 // validate form
+function validateForm(user, userEmail, userPassword, userPasswordConfirm) {
+    const errors = {}; 
+    
+    // --- INSERT ALL YOUR VALIDATION STAGES HERE ---
+    if (user.trim() === '') {
+        errors.user = 'Username is required.';
+    }
+    if (userPassword.length < 8) {
+        errors.password = 'Password must be at least 8 characters.';
+    }
+    // ... add all other checks (email format, password match, etc.) ...
+    
+    return errors;
+}
 
 
 async function createUser(){
 
     const url = 'http://localhost:3000/api/users/signup';
-
-    const user = userName.value;
-    const userEmail = email.value;
-    const userPassword = password.value;
-    const userPasswordConfirm = passwordConfirm.value;
-
-    // object to collect errors
-    const errors = {};
 
     try {
         const response = await fetch(url);
