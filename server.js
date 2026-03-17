19a9a216713791921166d76438e7189d97b55f3d
Nathan W <nwinslow@gmail.com>
2026-03-16 19:33:38 -0500

    updates

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+
+const app = express();
+app.use(express.json());
+
+const client = new MongoClient('mongodb://localhost:27017');
+await client.connect();
+const db = client.db('taskapp');
+const tasks = db.collection('tasks');
+
+// post comment
+
+app.listen(3000, () => console.log('Running on http://localhost:3000'));
