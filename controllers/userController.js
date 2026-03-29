630f95726f1de7a5a11bedcec3faa344863db249
Nathan W <nwinslow@gmail.com>
2026-03-28 21:40:21 -0500

    updated to use mongosse model.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,27 @@
-//
+import User from '../models/User.js';
+
+export function getUserByUsername(db) {
+  return async function (req, res) {
+    const { username } = req.query;
+
+    if (!username) {
+      return res.status(400).json({ error: 'Username required' });
+    }
+
+    try {
+      const user = await User.findOne({username});
+
+      if (!user) {
+        return res.json({ exists: false });
+      }
+
+      return res.json({
+        exists: true,
+        user
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Server error' });
+    }
+  };
+}
