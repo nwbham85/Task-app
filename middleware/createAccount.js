fee22b0b5b184d283b7dda33a02da8d689f2b3a0
Nathan W <nwinslow@gmail.com>
2026-07-13 13:52:44 -0500

    create account

--- middleware/createAccount.js
+++ middleware/createAccount.js
@@ -2,4 +2,21 @@ export function validate(email,username,password){
     if(!email || !username || !password) {
         console.log('field cannot be blank');
     }
+
+    //search username for symbols
+    const symbolRegex = /[^a-zA-Z0-9_-]/;
+
+    if (symbolRegex.test(username)) {
+        console.log("Username cannot contain symbols.");
+        return;
+        // Trigger your validation error here
+    }
+
+    if(password.value.length.trim() < 8) {
+        console.log('password must be 8 or more characters.');
+        return false;
+    }
+
 }
+
+export default validate;
